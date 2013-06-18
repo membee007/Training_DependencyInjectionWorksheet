efd1d1a419950ead49ea123579f56e13b7fb1772
Mark Pearl <markpearl@gmail.com>
2013-06-18 06:13:09 +0200

    Basic outline of tests to solve

---
+++ DependencyInjection_Worksheet/DependencyInjection_Worksheet/PublicHolidayNotifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DependencyInjection_Worksheet
+{
+    //
+    // You will need to refactor this class so you can test it properly
+    // You are not allowed to pass in primitives (i.e. ints)
+    // You are also not allow to edit the method signature of the
+    // GenerateNotification method
+    //
+    public class PublicHolidayNotifier
+    {
+        public string GenerateNotification()
+        {
+            if (DateTime.Now.Date == new DateTime(2013, 6, 17))
+                return "Today is Youth Day";
+            return "Today is a normal day";
+        }
+    }
+}
---
+++ DependencyInjection_Worksheet/DependencyInjection_Worksheet/PublicHolidayNotifierTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace DependencyInjection_Worksheet
+{
+    [TestFixture]
+    public class PublicHolidayNotifierTests
+    {
+        [Test]
+        public void IsYouthDayOnThe17June2013()
+        {
+        }
+
+        [Test]
+        public void IsNormalDayOnThe18June2013()
+        {
+            
+        }        
+    }
+}
